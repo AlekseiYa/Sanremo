3b08cd5f25f68752f0704918d76e9556945c4935
AlekseiYa <Aleksei_yanshin@icloud.com>
2024-03-29 22:54:17 +0300

    Work #1 29.03.24

---
+++ Python/main.py
@@ -0,0 +1,71 @@
+#Модуль 1 Введение 2 Задача 1
+
+#numberOne = int(input('Введите число 1 '))
+#numberTwo = int(input('Введите число 2 '))
+#numberThree = int(input('Введите число 3 '))
+#summ = numberOne + numberTwo + numberThree
+#print(summ)
+
+
+#Модуль 1 Введение 2 Задача 2
+
+#salary = int(input('Зарплата за месяц? '))
+#credit = int(input('Платеж по кредиту? '))
+#utility_bills = int(input('Какая задолжность по комунальнке? '))
+#summMoney = ((salary - credit) - utility_bills)
+#print("Остаток после всех расходов", summMoney)
+
+#Модуль 1 Введение 2 Задача 3
+
+#diagonalOne = int(input('Введите длинну первой диагонали'))
+#diagonalTwo = int(input('Введите длину второй диагонали'))
+#s = ((diagonalOne * diagonalTwo) / 2)
+#print('Площад ромба равна', s)
+
+#Модуль 1 Введение 2 Задача 4
+
+#print(' To be\n or not\n to be')
+
+#Модуль 1 Введение 2 Задача 5
+
+#print(' "Life is what happens \n        when \n           you are busy making other plans"     \n                                       John Lennon')
+
+#Модуль 1 Введение 3 Задача 1
+
+#num_one = (input('Введите число 1 '))
+#num_two = (input('Введите число 2 '))
+#num_three = (input('Введите число 3 '))
+#summNum = num_one + num_two + num_three
+#print('Сформерованное число', summNum)
+
+
+#Модуль 1 Введение 3 Задача 2
+#listNum = int(input('Введите черехзначное число '))
+#a = listNum % 10
+#b = listNum // 1000
+#c = listNum % 100 // 10
+#d = listNum // 100 % 10
+#sum = b * d * c * a
+#print('Результат произведениея равен ', sum)
+
+
+#Модуль 1 Введение 3 Задача 3
+#metrsNumber = int(input("Введите кол-во метров "))
+#convertSan = metrsNumber * 100
+#convertDcm = metrsNumber * 10
+#convertMm = metrsNumber * 1000
+#convertMili = metrsNumber * 0.00062137
+#print('Из', metrsNumber, 'получиться сантиметров', convertSan, 'децеметров', convertDcm, 'Милиметров', convertMm, 'Милей', convertMili)
+
+#Модуль 1 Введение 3 Задача 4
+
+#base_size = int(input('Введите размер основания треугольника '))
+#base_hide = int(input('Введите размер высоты '))
+#S = (base_size * base_hide) ** 2
+#print('Площадь треугольника равна', S)
+
+#Модуль 1 Введение 3 Задача 5
+
+#listNumber = (input('Введите черехзначное число '))
+#ret = (listNumber[::-1])
+#print(ret)
